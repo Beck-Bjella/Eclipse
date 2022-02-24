b19f9243b36e0c57ef958e673b67ad52efac4fe6
Beck Bjella <beckbjella@gmail.com>
2022-02-24 15:42:27 -0600

    update

--- lib/aimbot.py
+++ lib/aimbot.py
@@ -1,10 +1,10 @@
 import torch
-import math
 import win32api
 import ctypes
 import cv2 as cv
 import time
 import random
+
 
 PUL = ctypes.POINTER(ctypes.c_ulong)
 
@@ -50,13 +50,13 @@ class POINT(ctypes.Structure):
 class aimbot:
     screenshot_region = {'left': 752, 'top': 332, 'width': 416, 'height': 416}
     auto_fire_region = {'left': 20, 'right': 80, 'top': 0, 'bottom': 30}
-    aiming_status = "OFF"
+    aiming_status = "ON"
     running = True
     holding_shotgun = False
     extra = ctypes.c_ulong(0)
     ii_ = Input_I()
 
-    def __init__(self, model_confidence, model_iou, normal_scale, targeting_scale, mouse_delay, mouse_scale, auto_fire):
+    def __init__(self, model_confidence, model_iou, normal_scale, targeting_scale, mouse_delay):
         if not torch.cuda.is_available():
             print("")
             print("[ERROR] CUDA not available.")
@@ -68,8 +68,6 @@ class aimbot:
         self.normal_scale = normal_scale
         self.targeting_scale = targeting_scale
         self.mouse_delay = mouse_delay
-        self.mouse_scale = mouse_scale
-        self.auto_fire = auto_fire
 
         self.model = torch.hub.load('lib/yolov5-master/', 'custom', path='lib/weights.pt/', source='local')
         self.model.conf = model_confidence
@@ -106,7 +104,7 @@ class aimbot:
             confidence = detection["confidence"]
 
             cv.rectangle(image, x1y1, x2y2, (255, 255, 255), 2)
-            
+
             auto_fire_x1 = int(x1 + ((x2 - x1) * (self.auto_fire_region["left"] / 100)))
             auto_fire_x2 = int(x1 + ((x2 - x1) * (self.auto_fire_region["right"] / 100)))
             auto_fire_y1 = int(y1 + ((y2 - y1) * (self.auto_fire_region["top"] / 100)))
@@ -120,65 +118,35 @@ class aimbot:
             confidence_text = str(int(confidence * 100)) + "%"
             cv.putText(image, text=confidence_text, org=(x1, y1 - 7), fontFace=cv.FONT_HERSHEY_SIMPLEX, fontScale=0.65, color=(255, 255, 255), thickness=2, lineType=cv.LINE_AA)
 
-        fps = detection["fps"]
-
-        fps_text = "FPS: " + str(fps)
-        cv.putText(image, text=fps_text, org=(5, 30), fontFace=cv.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2, lineType=cv.LINE_AA)
-
-        cv.putText(image, text=aimbot.aiming_status, org=(5, 65), fontFace=cv.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2, lineType=cv.LINE_AA)
-
         return image
-
-    def shotgun_auto_fire(self, detection):
-        if self.auto_fire:
-            if self.aiming_status == "ON":
-                if self.holding_shotgun:
-                    if detection["x1y1"]:
-                        x1, y1 = detection["x1y1"]
-                        x2, y2 = detection["x2y2"]
-                        absolute_x1, absolute_y1 = aimbot.screenshot_region["left"] + x1, aimbot.screenshot_region["top"] + y1
-                        absolute_x2, absolute_y2 = aimbot.screenshot_region["left"] + x2, aimbot.screenshot_region["top"] + y2
-
-                        auto_fire_x1 = int(absolute_x1 + ((absolute_x2 - absolute_x1) * (self.auto_fire_region["left"] / 100)))
-                        auto_fire_x2 = int(absolute_x1 + ((absolute_x2 - absolute_x1) * (self.auto_fire_region["right"] / 100)))
-                        auto_fire_y1 = int(absolute_y1 + ((absolute_y2 - absolute_y1) * (self.auto_fire_region["top"] / 100)))
-                        auto_fire_y2 = int(absolute_y1 + ((absolute_y2 - absolute_y1) * (self.auto_fire_region["bottom"] / 100)))
-
-                        if auto_fire_x1 < 960 and auto_fire_x2 > 960:
-                            if auto_fire_y1 < 540 and auto_fire_y2 > 540:
-                                ctypes.windll.user32.mouse_event(0x0002)
-                                self.sleep(random.uniform(1, 6) / 1000)
-                                ctypes.windll.user32.mouse_event(0x0004)
 
     def move_crosshair(self, detection):
         if detection["x1y1"]:
-            if aimbot.aiming_status == "ON":
-                absolute_head = aimbot.screenshot_region["left"] + detection["head"][0], aimbot.screenshot_region["top"] + detection["head"][1]
+            absolute_head = aimbot.screenshot_region["left"] + detection["head"][0], aimbot.screenshot_region["top"] + detection["head"][1]
 
-                right_state = win32api.GetKeyState(0x02)
-                if right_state in (-127, -128):
-                    scale = self.targeting_scale * self.mouse_scale
-                else:
-                    scale = self.normal_scale * self.mouse_scale
+            right_state = win32api.GetKeyState(0x02)
+            if right_state in (-127, -128):
+                scale = self.targeting_scale
+            else:
+                scale = self.normal_scale
 
-                rel_x = int((absolute_head[0] - 960) * scale)
-                rel_y = int((absolute_head[1] - 540) * scale)
+            rel_x = int((absolute_head[0] - 960) * scale)
+            rel_y = int((absolute_head[1] - 540) * scale)
 
-                aimbot.ii_.mi = MouseInput(rel_x, rel_y, 0, 0x0001, 0, ctypes.pointer(aimbot.extra))
-                input_obj = Input(ctypes.c_ulong(0), aimbot.ii_)
-                ctypes.windll.user32.SendInput(1, ctypes.byref(input_obj), ctypes.sizeof(input_obj))
+            aimbot.ii_.mi = MouseInput(rel_x, rel_y, 0, 0x0001, 0, ctypes.pointer(aimbot.extra))
+            input_obj = Input(ctypes.c_ulong(0), aimbot.ii_)
+            ctypes.windll.user32.SendInput(1, ctypes.byref(input_obj), ctypes.sizeof(input_obj))
 
-                aimbot.sleep(self.mouse_delay)
+            aimbot.sleep(self.mouse_delay)
 
     def inference(self, image):
         best_detection = {}
-        start_time = time.time()
 
         raw_results = self.model(image, 140)
         results = raw_results.xyxy[0]
 
         if len(results) > 0:
-            closest_detection = False
+            closest_detection = 50
 
             for x in range(len(results)):
                 x1 = int(float(results[x][0]))
@@ -195,20 +163,25 @@ class aimbot:
 
                 exclusion_zone = x1 < 15
 
-                detection_distance = abs(math.dist((208, 208), head))
+                detection_distance = ((y2 - y1) * 2) + ((x2 - x1) * 2)
 
-                if not closest_detection:
-                    closest_detection = 208
-
-                if not exclusion_zone and detection_distance < closest_detection:
+                if not exclusion_zone and detection_distance > closest_detection:
                     closest_detection = detection_distance
                     best_detection = {'x1y1': x1y1, 'x2y2': x2y2, 'head': head, 'confidence': confidence}
 
-        fps = int(1 / (time.time() - start_time))
-
         if best_detection:
-            best_detection.update({'fps': fps})
+            pass
         else:
-            best_detection.update({'x1y1': False, 'x2y2': False, 'head': False, 'confidence': False, 'fps': fps})
+            best_detection.update({'x1y1': False, 'x2y2': False, 'head': False, 'confidence': False})
 
         return best_detection
+
+    def main(self, image):
+        detection = self.inference(image)
+
+        if aimbot.aiming_status == "ON":
+            self.move_crosshair(detection)
+
+        results = self.draw_on_image(image, detection)
+
+        return results
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import time
+import numpy as np
+import mss
 from lib.aimbot import aimbot
 import tkinter as tk
 from tkinter import ttk
 import json
 from pynput import keyboard
-import numpy as np
-import mss
 import cv2 as cv
 import os
 
@@ -86,81 +87,40 @@ class eclipse_menu(tk.Tk):
         eclipse_text = tk.Label(self, text=eclipse_string, font=("Courier", 8))
         eclipse_text.place(x=135, y=0)
 
-        self.xy_sensitivity_input = ttk.Entry(self)
-        self.xy_sensitivity_input.place(x=430, y=135)
-        xy_sensitivity_label = ttk.Label(self, text="XY Sensitivity")
-        xy_sensitivity_label.place(x=430, y=115)
+        self.normal_scale = 0
+        self.normal_scale_slider = ttk.Scale(self, from_=0, to=100, length=600, command=self.update_normal_scale_slider, orient='horizontal')
+        self.normal_scale_slider.place(x=60, y=200)
+        self.normal_scale_label = ttk.Label(self, text="Normal Scale: ")
+        self.normal_scale_label.place(x=312, y=180)
 
-        self.targeting_sensitivity_input = ttk.Entry(self)
-        self.targeting_sensitivity_input.place(x=430, y=190)
-        targeting_sensitivity_label = ttk.Label(self, text="Targeting Sensitivity")
-        targeting_sensitivity_label.place(x=430, y=170)
-
-        slider_warning_label = ttk.Label(self, text="")
-        slider_warning_label.place(x=45, y=190)
-
-        self.mouse_scale = 0
-        self.mouse_scale_slider = ttk.Scale(self, from_=50, to=150, length=600, command=self.update_mouse_scale_slider, orient='horizontal')
-        self.mouse_scale_slider.place(x=60, y=310)
-        self.mouse_scale_label = ttk.Label(self, text="Mouse Scale: ")
-        self.mouse_scale_label.place(x=312, y=290)
+        self.targeting_scale = 0
+        self.targeting_scale_slider = ttk.Scale(self, from_=0, to=100, length=600, command=self.update_targeting_scale_slider, orient='horizontal')
+        self.targeting_scale_slider.place(x=60, y=310)
+        self.targeting_scale_label = ttk.Label(self, text="Targeting Scale: ")
+        self.targeting_scale_label.place(x=312, y=290)
 
         self.start_button = ttk.Button(text="Start", command=self.start_button_press)
         self.start_button.place(x=320, y=130)
 
-        self.auto_fire = tk.BooleanVar()
-        self.auto_fire_toggle = ttk.Checkbutton(text="Enable shotgun trigger bot", onvalue=True, offvalue=False, variable=self.auto_fire)
-        self.auto_fire_toggle.place(x=80, y=115)
-
-        self.slot_1_hotkey_input = ttk.Entry(self)
-        self.slot_1_hotkey_input.place(x=80, y=140)
-
-        self.slot_2_hotkey_input = ttk.Entry(self)
-        self.slot_2_hotkey_input.place(x=80, y=170)
-
-        self.slot_3_hotkey_input = ttk.Entry(self)
-        self.slot_3_hotkey_input.place(x=80, y=200)
-
-        self.slot_4_hotkey_input = ttk.Entry(self)
-        self.slot_4_hotkey_input.place(x=80, y=230)
-
-        self.slot_5_hotkey_input = ttk.Entry(self)
-        self.slot_5_hotkey_input.place(x=80, y=260)
-
-        self.shotgun_slot = tk.StringVar()
-        radio_button_names = {"Item Slot 1": 1,
-                              "Item Slot 2": 2,
-                              "Item Slot 3": 3,
-                              "Item Slot 4": 4,
-                              "Item Slot 5": 5}
-        position_y = 140
-        for (text, value) in radio_button_names.items():
-            ttk.Radiobutton(self, text=text, variable=self.shotgun_slot, value=value).place(x=210, y=position_y)
-            position_y += 30
-
         self.load_gui_data()
 
-    def update_mouse_scale_slider(self, event):
-        self.mouse_scale = float(round((self.mouse_scale_slider.get() / 100), 2))
-        self.mouse_scale_label.configure(text="Mouse Scale: " + str(self.mouse_scale))
+    def update_normal_scale_slider(self, event):
+        self.normal_scale = float(round((self.normal_scale_slider.get() / 100), 2))
+        self.normal_scale_label.configure(text="Normal Scale: " + str(self.normal_scale))
+
+    def update_targeting_scale_slider(self, event):
+        self.targeting_scale = float(round((self.targeting_scale_slider.get() / 100), 2))
+        self.targeting_scale_label.configure(text="Targeting Scale: " + str(self.targeting_scale))
 
     def load_gui_data(self):
         if os.path.isfile("lib/config.json"):
             config_data = get_config_file()
 
-            self.xy_sensitivity_input.insert(0, config_data["xy_sensitivity"])
-            self.targeting_sensitivity_input.insert(0, config_data["targeting_sensitivity"])
-            self.mouse_scale_slider.set(int(config_data["mouse_scale"] * 100))
-            self.auto_fire.set(config_data["auto_fire"])
-            self.slot_1_hotkey_input.insert(0, config_data["slot_1_hotkey"])
-            self.slot_2_hotkey_input.insert(0, config_data["slot_2_hotkey"])
-            self.slot_3_hotkey_input.insert(0, config_data["slot_3_hotkey"])
-            self.slot_4_hotkey_input.insert(0, config_data["slot_4_hotkey"])
-            self.slot_5_hotkey_input.insert(0, config_data["slot_5_hotkey"])
-            self.shotgun_slot.set(str(config_data["shotgun_slot"]))
+            self.normal_scale_slider.set(int(config_data["normal_scale"] * 100))
+            self.targeting_scale_slider.set(int(config_data["targeting_scale"] * 100))
         else:
-            self.mouse_scale_slider.set(100)
-            self.auto_fire.set(False)
+            self.normal_scale_slider.set(50)
+            self.targeting_scale_slider.set(50)
 
     def start_button_press(self):
         global run_aimbot
@@ -169,23 +129,8 @@ class eclipse_menu(tk.Tk):
         if os.path.isfile("lib/config.json"):
             os.remove("lib/config.json")
 
-        xy_sensitivity = float(self.xy_sensitivity_input.get())
-        targeting_sensitivity = float(self.targeting_sensitivity_input.get())
-        normal_scale = (10 / float(xy_sensitivity))
-        targeting_scale = (10 / (float(xy_sensitivity) * ((float(targeting_sensitivity)) / 100)))
-
-        config_data = {"xy_sensitivity": xy_sensitivity,
-                       "targeting_sensitivity": targeting_sensitivity,
-                       "normal_scale": normal_scale,
-                       "targeting_scale": targeting_scale,
-                       "mouse_scale": float(self.mouse_scale),
-                       "auto_fire": self.auto_fire.get(),
-                       "slot_1_hotkey": self.slot_1_hotkey_input.get(),
-                       "slot_2_hotkey": self.slot_2_hotkey_input.get(),
-                       "slot_3_hotkey": self.slot_3_hotkey_input.get(),
-                       "slot_4_hotkey": self.slot_4_hotkey_input.get(),
-                       "slot_5_hotkey": self.slot_5_hotkey_input.get(),
-                       "shotgun_slot": int(self.shotgun_slot.get())}
+        config_data = {"normal_scale": float(self.normal_scale),
+                       "targeting_scale": float(self.targeting_scale)}
 
         with open('lib/config.json', 'w', encoding='utf-8') as f:
             json.dump(config_data, f, ensure_ascii=False, indent=4)
@@ -201,14 +146,7 @@ if __name__ == "__main__":
 
     if run_aimbot:
         config_file = get_config_file()
-        aimbot = aimbot(0.5, 0.01, config_file["normal_scale"], config_file["targeting_scale"], 0.0001, config_file["mouse_scale"], config_file["auto_fire"])
-
-        slot_1_hotkey = config_file["slot_1_hotkey"]
-        slot_2_hotkey = config_file["slot_2_hotkey"]
-        slot_3_hotkey = config_file["slot_3_hotkey"]
-        slot_4_hotkey = config_file["slot_4_hotkey"]
-        slot_5_hotkey = config_file["slot_5_hotkey"]
-        shotgun_slot = config_file["shotgun_slot"]
+        aimbot = aimbot(0.5, 1, config_file["normal_scale"], config_file["targeting_scale"], 0.0001)
 
         listener = keyboard.Listener(on_release=on_key_release, on_press=on_key_press)
         listener.start()
@@ -216,15 +154,9 @@ if __name__ == "__main__":
         while aimbot.running:
             screenshot = np.array(mss.mss().grab(aimbot.screenshot_region))
 
-            detection = aimbot.inference(screenshot)
+            results = aimbot.main(screenshot)
 
-            aimbot.move_crosshair(detection)
-
-            aimbot.shotgun_auto_fire(detection)
-
-            screenshot = aimbot.draw_on_image(screenshot, detection)
-
-            cv.imshow("Eclipse", screenshot)
+            cv.imshow("Eclipse", results)
 
             if cv.waitKey(1) == ord('q'):
                 cv.destroyAllWindows()
