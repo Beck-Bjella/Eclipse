6890d528d07da10b33b5a7502982565354f30615
Beck Bjella <beckbjella@gmail.com>
2024-10-30 10:22:17 -0500

    add documentation

--- Main.py
+++ Main.py
@@ -9,6 +9,9 @@ import cv2
 
 
 def on_key_release(key):
+    """
+    Handles key release events.
+    """
     try:
         if key == keyboard.Key.f2:
             aimbot.update_aimimg_status("OFF")
@@ -19,6 +22,9 @@ def on_key_release(key):
 
 
 def on_key_press(key):
+    """
+    Handles key press events.
+    """
     try:
         if key == keyboard.Key.f2:
             aimbot.update_aimimg_status("ON")
@@ -28,6 +34,9 @@ def on_key_press(key):
 
 
 def get_config_file():
+    """
+    Loads an existing configuration file.
+    """
     if not os.path.isfile("lib/config.json"):
         return False
     else:
@@ -37,217 +46,178 @@ def get_config_file():
         return config_data
 
 
-def create_default_config_file():
+def create_config_file():
+    """
+    Creates a configuration file with temporary settings.
+    """
     if os.path.isfile("lib/config.json"):
         os.remove("lib/config.json")
 
-    config_data = {"normal_scale": 0.1,
-                   "targeting_scale": 0.2}
+    config_data = {"normal_scale": 0.1, "targeting_scale": 0.2}
 
     with open('lib/config.json', 'w', encoding='utf-8') as f:
         json.dump(config_data, f, ensure_ascii=False, indent=4)
 
 
 def update_config_file():
-    print("[?] Updating file")
-    print("")
+    """
+    Updates the configuration file with inputted values.
+    """
+    print("[?] Updating file\n")
 
     config_data = get_config_file()
-
     normal_scale = config_data["normal_scale"]
     targeting_scale = config_data["targeting_scale"]
     print(f"Current Normal Scale: {normal_scale}")
-    print(f"Current Targeting Scale: {normal_scale}")
-    print()
+    print(f"Current Targeting Scale: {normal_scale}\n")
 
+    # Update normal scale with user input
     valid_input = False
     while not valid_input:
         new_normal_scale = input("Enter a new normal scale: ")
-
         try:
             normal_scale = float(new_normal_scale)
             valid_input = True
-
         except ValueError:
-            print("[!] Invalid input")
+            print("[!] Invalid input\n")
 
-        print("")
-
+    # Update targeting scale with user input
     valid_input = False
     while not valid_input:
         new_targeting_scale = input("Enter a new targeting scale: ")
-
         try:
             targeting_scale = float(new_targeting_scale)
             valid_input = True
-
         except ValueError:
-            print("[!] Invalid input")
+            print("[!] Invalid input\n")
 
-        print("")
+    config_data = {"normal_scale": normal_scale, "targeting_scale": targeting_scale}
 
-    print("----------------------------------------")
-    print("")
-
-    config_data = {"normal_scale": normal_scale,
-                   "targeting_scale": targeting_scale}
-
+    # Save new configuration
     with open('lib/config.json', 'w', encoding='utf-8') as f:
         json.dump(config_data, f, ensure_ascii=False, indent=4)
 
 
 if __name__ == "__main__":
+    """
+    Starting point of the program. Handles main menu and input logic.
+    """
     print(r" ____________________ .____    ._____________  ____________________")
     print(r"\_   _____/\_   ___ \|    |   |   \______   \/   _____/\_   _____/")
     print(r" |    __)_ /    \  \/|    |   |   ||     ___/\_____  \  |    __)_")
     print(r" |        \\     \___|    |___|   ||    |    /        \ |        \ ")
     print(r"/_______  / \______  /_______ \___||____|   /_______  //_______  /")
     print(r"        \/         \/        \/                     \/         \/")
-    print("                     (Neural Network Aimbot)")
-    print("")
+    print("                     (Neural Network Aimbot)\n")
 
+    # User options
     print("[1] Type 'R' to run the aimbot")
     print("[2] Type 'E' to edit/create a configuration file")
     print("[3] Type 'H' for instructions/help")
-    print("[4] Type 'Q' to quit the program")
-    print("")
+    print("[4] Type 'Q' to quit the program\n")
 
+    # User input
     option = None
-
     valid_input = False
     while not valid_input:
-        option = input("Please select a option: ")
+        option = input("Please select an option: ")
 
-        if option == "Q" or option == "E" or option == "R" or option == "H":
+        if option in {"Q", "E", "R", "H"}:
             valid_input = True
         else:
-            print("[!] Invalid input")
-        print("")
+            print("[!] Invalid input\n")
 
-    print("----------------------------------------")
-    print("")
+    print("----------------------------------------\n")
 
+    # Handle user input
     match option:
-        case "Q":
+        case "Q":  # Quit program
             print("Quitting... ")
             time.sleep(3)
             quit()
 
-        case "E":
+        case "E":  # Edit or create configuration file
             if not get_config_file():
-                create_default_config_file()
-
-                print("[!] No Configuration file found")
-                print("")
-                print("[?] Creating file...")
-                print("")
+                create_config_file()
+                print("[!] No Configuration file found\n")
+                print("[?] Creating file...\n")
                 time.sleep(3)
-                print("----------------------------------------")
-                print("")
-                print("[?] New configuration file created")
-                print("")
-                print("----------------------------------------")
-                print("")
+                print("----------------------------------------\n")
+                print("[?] New configuration file created\n")
+                print("----------------------------------------\n")
                 print("Quitting...")
                 time.sleep(3)
                 exit()
-
             else:
                 update_config_file()
-
-                print("[?] New configuration file successfully created")
-                print("")
-                print("----------------------------------------")
-                print("")
+                print("[?] Configuration file successfully updated\n")
+                print("----------------------------------------\n")
                 print("Quitting...")
                 time.sleep(3)
                 exit()
 
-        case "H":
+        case "H":  # Help and instructions
             print("[?] Aimbot Calibration")
-            print("    - If the aimbot is barely moving then you need to increase the sensitivity")
-            print("    - If the aimbot is oscillating around a target then you need to lower the sensitivity")
-            print("")
-            print("    - General rules")
-            print("        - Quick/snappy movements = too high sensitivity")
-            print("        - Slow/no movements = too low sensitivity")
-            print("")
-            print("    - Sensitivity scales from (0.0 - 1.0)")
-            print("    - It's recommended to change sensitivity in chucks of 0.1")
-            print("")
-            print("    - The value that works best for you is based on your")
-            print("        - Mouse DPI")
-            print("        - Game Sensitivity")
-            print("")
+            print("    - Adjust sensitivity to improve aim accuracy")
+            print("    - General rules: Quick/snappy movements = too high sensitivity")
+            print("                      Slow/no movements = too low sensitivity")
+            print("    - Recommended sensitivity adjustment in increments of 0.1\n")
+            print("    - Optimal sensitivity depends on factors like Mouse DPI and Game Sensitivity\n")
             print("[?] Hardware Requirements")
-            print("    - You must have a NVIDIA GPU")
-            print("    - You must be on a Windows Computer")
-            print("")
-            print("[?] Optimisations")
-            print("    - Close other tabs on your computer")
-            print("")
-            print("    - The faster your GPU the better the aimbot will work")
-            print("")
-            print("    - The lower your graphics in the game are the faster the aimbot will run")
-            print("        - This can make a huge difference")
-            print("")
+            print("    - Requires an NVIDIA GPU on a Windows Computer\n")
+            print("[?] Optimizations")
+            print("    - Close unnecessary tabs for improved performance\n")
+            print("    - Lower in-game graphics settings for faster aimbot performance\n")
             print("[?] Press any key to continue")
-
-            wait = input("")
-
-            print("----------------------------------------")
-            print("")
+            input("")  # Pause for user to review instructions
+            print("----------------------------------------\n")
             print("Quitting...")
             time.sleep(3)
             exit()
 
-        case "R":
+        case "R":  # Run aimbot
+            # Load configuration file
             if not get_config_file():
-                print("[!] Must have a configuration file to run the aimbot")
-                print("")
-                print("----------------------------------------")
-                print("")
+                print("[!] Must have a configuration file to run the aimbot\n")
+                print("----------------------------------------\n")
                 print("Quitting...")
                 time.sleep(3)
                 exit()
 
+            # Initialize aimbot
             config_file = get_config_file()
             aimbot = Aimbot(0.5, 0.45, config_file["normal_scale"], config_file["targeting_scale"], "1920x1080")
             time.sleep(1)
-            print("")
-
-            print("----------------------------------------")
-            print("")
-            print("[?] Aimbot successfully activated")
-            print("")
+            print("\n----------------------------------------\n")
+            print("[?] Aimbot successfully activated\n")
             print("[1] Hold f2 to enable the aimbot")
-            print("[2] Press f3 to quit")
-            print("")
+            print("[2] Press f3 to quit\n")
 
+            # Setup key listeners
             listener = keyboard.Listener(on_release=on_key_release, on_press=on_key_press)
             listener.start()
 
+            # Main capture loop
             sct = mss.mss()
-            last = None
             while aimbot.running:
                 screenshot = np.array(sct.grab(aimbot.screenshot_region))
 
+                # Distance based detection
                 detection = aimbot.inference_dist(screenshot)
-
                 if aimbot.visualize:
                     drawn = aimbot.draw_detection(screenshot, detection)
-
-                    cv2.imshow('Eclipse', drawn)
+                    cv2.imshow('Aimbot Visualization', drawn)
                     cv2.waitKey(1)
 
+                # Move crosshair
                 if aimbot.aiming_status == "ON":
                     aimbot.move_crosshair(detection)
 
+            # Close Open CV windows
             if aimbot.visualize:
                 cv2.destroyAllWindows()
 
-            print("----------------------------------------")
-            print("")
+            print("----------------------------------------\n")
             print("Quitting...")
             time.sleep(3)
             exit()
--- lib/Aimbot.py
+++ lib/Aimbot.py
@@ -1,3 +1,4 @@
+# Imports
 import ctypes
 import math
 import time
@@ -5,49 +6,71 @@ import torch
 import win32api
 import cv2
 
-
 PUL = ctypes.POINTER(ctypes.c_ulong)
 
 
 class KeyBdInput(ctypes.Structure):
-    _fields_ = [("wVk", ctypes.c_ushort),
-                ("wScan", ctypes.c_ushort),
-                ("dwFlags", ctypes.c_ulong),
-                ("time", ctypes.c_ulong),
-                ("dwExtraInfo", PUL)]
+    """
+    Data structure for keyboard input events, used to simulate keyboard actions.
+    """
+    _fields_ = [("wVk", ctypes.c_ushort),  # Virtual-key code
+                ("wScan", ctypes.c_ushort),  # Hardware scan code
+                ("dwFlags", ctypes.c_ulong),  # Flags for the event type
+                ("time", ctypes.c_ulong),  # Timestamp for the event
+                ("dwExtraInfo", PUL)]  # Additional information
 
 
 class HardwareInput(ctypes.Structure):
-    _fields_ = [("uMsg", ctypes.c_ulong),
-                ("wParamL", ctypes.c_short),
-                ("wParamH", ctypes.c_ushort)]
+    """
+    Data structure for hardware input events, used to simulate specific hardware actions.
+    """
+    _fields_ = [("uMsg", ctypes.c_ulong),  # Message identifier
+                ("wParamL", ctypes.c_short),  # Low word parameter
+                ("wParamH", ctypes.c_ushort)]  # High word parameter
 
 
 class MouseInput(ctypes.Structure):
-    _fields_ = [("dx", ctypes.c_long),
-                ("dy", ctypes.c_long),
-                ("mouseData", ctypes.c_ulong),
-                ("dwFlags", ctypes.c_ulong),
-                ("time", ctypes.c_ulong),
-                ("dwExtraInfo", PUL)]
+    """
+    Data structure for mouse input events, used to simulate mouse movements and clicks.
+    """
+    _fields_ = [("dx", ctypes.c_long),  # x-coordinate of movement
+                ("dy", ctypes.c_long),  # y-coordinate of movement
+                ("mouseData", ctypes.c_ulong),  # Additional mouse event data
+                ("dwFlags", ctypes.c_ulong),  # Flags for event type
+                ("time", ctypes.c_ulong),  # Timestamp for the event
+                ("dwExtraInfo", PUL)]  # Additional information
 
 
 class Input_I(ctypes.Union):
-    _fields_ = [("ki", KeyBdInput),
-                ("mi", MouseInput),
-                ("hi", HardwareInput)]
+    """
+    Union of different input structures, allowing a single input to represent
+    keyboard, mouse, or hardware input events.
+    """
+    _fields_ = [("ki", KeyBdInput),  # Keyboard input
+                ("mi", MouseInput),  # Mouse input
+                ("hi", HardwareInput)]  # Hardware input
 
 
 class Input(ctypes.Structure):
-    _fields_ = [("type", ctypes.c_ulong),
-                ("ii", Input_I)]
+    """
+    Main input structure, used to send input events via the Windows API.
+    """
+    _fields_ = [("type", ctypes.c_ulong),  # Input event type
+                ("ii", Input_I)]  # Input data
 
 
 class POINT(ctypes.Structure):
+    """
+    Structure representing a point on the screen, with x and y coordinates.
+    """
     _fields_ = [("x", ctypes.c_long), ("y", ctypes.c_long)]
 
 
 class Aimbot:
+    """
+    Aimbot class to perform object detection on screen regions and simulate
+    mouse movements for aiming at detected objects.
+    """
     aiming_status = "OFF"
     running = True
     visualize = False
@@ -55,35 +78,44 @@ class Aimbot:
     ii_ = Input_I()
 
     def __init__(self, model_confidence, model_iou, normal_scale, targeting_scale, window_size):
+        """
+        Initializes the Aimbot with configuration for object detection and screen regions.
+        """
+        # Check for CUDA
         if not torch.cuda.is_available():
             print("[!] CUDA not available.")
-            print("")
             print("Quitting...")
             time.sleep(3)
             exit()
 
+        # Screen region based on window size
         match window_size:
             case "1920x1080":
-                self.screenshot_region = {'left': 752, 'top': 332, 'width': 416, 'height': 416}  # 1920x1080
-
+                self.screenshot_region = {'left': 752, 'top': 332, 'width': 416, 'height': 416}
             case "1280x720":
-                self.screenshot_region = {'left': 432, 'top': 152, 'width': 416, 'height': 416}  # 1280x720
-
+                self.screenshot_region = {'left': 432, 'top': 152, 'width': 416, 'height': 416}
             case _:
-                self.screenshot_region = {'left': 752, 'top': 332, 'width': 416, 'height': 416}  # 1920x1080
+                self.screenshot_region = {'left': 752, 'top': 332, 'width': 416, 'height': 416}
 
         self.normal_scale = normal_scale
         self.targeting_scale = targeting_scale
 
+        # Load the YOLOv5 model
         self.model = torch.hub.load('ultralytics/yolov5', 'custom', path='lib/weights.pt/', force_reload=True)
         self.model.conf = model_confidence
         self.model.iou = model_iou
 
     def update_aimimg_status(self, updated_value):
+        """
+        Updates the aiming status.
+        """
         self.aiming_status = updated_value
 
     @staticmethod
     def sleep(duration):
+        """
+        Sleep function for very precise time intervals.
+        """
         get_now = time.perf_counter
         now = get_now()
         end = now + duration
@@ -91,53 +123,49 @@ class Aimbot:
             now = get_now()
 
     def move_crosshair(self, detection):
+        """
+        Moves the crosshair based on the detected object's location.
+        """
         if detection["x1y1"]:
             absolute_head = self.screenshot_region["left"] + detection["head"][0], self.screenshot_region["top"] + detection["head"][1]
 
+            # Adjust scale based on held right mouse button
             right_state = win32api.GetKeyState(0x02)
-            if right_state in (-127, -128):
-                scale = self.targeting_scale
-            else:
-                scale = self.normal_scale
+            scale = self.targeting_scale if right_state in (-127, -128) else self.normal_scale
 
+            # Calculate relative movements
             distance_x = int(absolute_head[0] - 960)
             distance_y = int(absolute_head[1] - 540)
-
             rel_x = int(distance_x * scale)
             rel_y = int(distance_y * scale)
 
+            # Move mouse
             self.ii_.mi = MouseInput(rel_x, rel_y, 0, 0x0001, 0, ctypes.pointer(self.extra))
             input_obj = Input(ctypes.c_ulong(0), self.ii_)
             ctypes.windll.user32.SendInput(1, ctypes.byref(input_obj), ctypes.sizeof(input_obj))
 
     def inference_conf(self, image):
+        """
+        Performs object detection and returns the best detection based on confidence.
+        """
         best_detection = {}
-
         raw_results = self.model(image)
         results = raw_results.xyxy[0]
 
         if len(results) > 0:
             highest_confidence = 0
-
             for x in range(len(results)):
-                x1 = int(float(results[x][0]))
-                y1 = int(float(results[x][1]))
-                x2 = int(float(results[x][2]))
-                y2 = int(float(results[x][3]))
-
-                x1y1 = x1, y1
-                x2y2 = x2, y2
-
+                # Extract bounding box coordinates and calculate head position
+                x1, y1 = int(float(results[x][0])), int(float(results[x][1]))
+                x2, y2 = int(float(results[x][2])), int(float(results[x][3]))
                 head = int(x1 + (abs(x1 - x2) / 2)), int(y1 + (abs(y1 - y2) / 3))
-
                 confidence = results[x][4].item()
-                detection_distance = math.dist((208, 208), (head[0], head[1]))
+                detection_distance = math.dist((208, 208), head)
 
                 exclusion_zone = x1 < 40
-
                 if not exclusion_zone and confidence > highest_confidence:
                     highest_confidence = confidence
-                    best_detection = {'x1y1': x1y1, 'x2y2': x2y2, 'head': head, 'confidence': confidence, 'distance': detection_distance}
+                    best_detection = {'x1y1': (x1, y1), 'x2y2': (x2, y2), 'head': head, 'confidence': confidence, 'distance': detection_distance}
 
         if not best_detection:
             best_detection.update({'x1y1': False, 'x2y2': False, 'head': False, 'confidence': False, 'distance': False})
@@ -145,33 +173,27 @@ class Aimbot:
         return best_detection
 
     def inference_dist(self, image):
+        """
+        Performs object detection and returns the closest detection based on distance.
+        """
         best_detection = {}
-
         raw_results = self.model(image)
         results = raw_results.xyxy[0]
 
         if len(results) > 0:
             closest_detection = 10000
-
             for x in range(len(results)):
-                x1 = int(float(results[x][0]))
-                y1 = int(float(results[x][1]))
-                x2 = int(float(results[x][2]))
-                y2 = int(float(results[x][3]))
-
-                x1y1 = x1, y1
-                x2y2 = x2, y2
-
+                # Extract bounding box coordinates and calculate head position
+                x1, y1 = int(float(results[x][0])), int(float(results[x][1]))
+                x2, y2 = int(float(results[x][2])), int(float(results[x][3]))
                 head = int(x1 + (abs(x1 - x2) / 2)), int(y1 + (abs(y1 - y2) / 3.9))
-
                 confidence = results[x][4].item()
-                detection_distance = math.dist((208, 208), (head[0], head[1]))
+                detection_distance = math.dist((208, 208), head)
 
                 exclusion_zone = x1 < 40
-
                 if not exclusion_zone and detection_distance < closest_detection:
                     closest_detection = detection_distance
-                    best_detection = {'x1y1': x1y1, 'x2y2': x2y2, 'head': head, 'confidence': confidence, 'distance': detection_distance}
+                    best_detection = {'x1y1': (x1, y1), 'x2y2': (x2, y2), 'head': head, 'confidence': confidence, 'distance': detection_distance}
 
         if not best_detection:
             best_detection.update({'x1y1': False, 'x2y2': False, 'head': False, 'confidence': False, 'distance': False})
@@ -180,21 +202,19 @@ class Aimbot:
 
     @staticmethod
     def draw_detection(image, detection):
+        """
+        Draws bounding boxes and detection information on an image.
+        """
         if detection["x1y1"]:
-            x1y1 = detection["x1y1"]
-            x2y2 = detection["x2y2"]
-            x1, y1 = detection["x1y1"]
-            head = detection["head"]
-            confidence = detection["confidence"]
+            x1y1, x2y2, head, confidence = detection["x1y1"], detection["x2y2"], detection["head"], detection["confidence"]
 
+            # Draw bounding box
             cv2.rectangle(image, x1y1, x2y2, (255, 0, 0), 2)
-
             cv2.line(image, (208, 208), head, (255, 0, 0), 2)
-
             cv2.circle(image, head, 5, (255, 0, 0), -1)
 
+            # Draw confidence text
             confidence_text = str(int(confidence * 100)) + "%"
-            cv2.putText(image, text=confidence_text, org=(x1, y1 - 7), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.65, color=(255, 0, 0), thickness=2, lineType=cv2.LINE_AA)
+            cv2.putText(image, text=confidence_text, org=(x1y1[0], x1y1[1] - 7), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.65, color=(255, 0, 0), thickness=2, lineType=cv2.LINE_AA)
 
         return image
-    
